97653e3b9070adbadfa87d1c6ac47b13c04c9d0a
Rune Sørensen <runesoerensen@gmail.com>
2010-07-17 02:58:30 +0200

    Set permissions for couchdb and install rather than upgrade

--- cookbooks/couchdb/recipes/default.rb
+++ cookbooks/couchdb/recipes/default.rb
@@ -1,5 +1,12 @@
-package "couchdb" do
+package "dev-db/couchdb" do
   package_name "dev-db/couchdb"
-  action :upgrade
+  action :install
   version "0.9.1"
+end
+
+directory "/var/run/couchdb" do
+  owner 'couchdb'
+  group 'couchdb'
+  mode 0755
+  recursive true
 end
